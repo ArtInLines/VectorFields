fd283a6e12eca368bb253593a2419d8e0e24b4b4
ArtInLines <linesinart@gmail.com>
2023-10-18 08:33:38 +0200

    Add '-d' flag for debugging

--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@ PROD_FLAGS="-O2 -s"
 DEV_FLAGS="-g -ggdb"
 CFLAGS="-Wall -Wextra -Wimplicit -Wpedantic -Wno-unused-function -std=c99"
 
-
 LIB_PATHS="-L./bin"
 INCLUDES="-I./deps/raylib/src -I./deps/stb -I./deps/artinlines"
 RAYLIB_DEP="-lraylib -lopengl32 -lgdi32 -lwinmm"
 DEPS="$INCLUDES $LIB_PATHS $RAYLIB_DEP"
 
-if [[ $1 -eq "a" ]] || [ -d "./bin" ]; then
+if [[ $1 == "a" ]] || [ ! -d "./bin" ]; then
 	# Remove old bin folder
 	rm -rf "./bin"
 	mkdir "./bin"
@@ -22,4 +21,9 @@ fi
 if [ -f "./bin/VectorFields" ]; then
 	rm -f "./bin/VectorFields"
 fi
-gcc $CFLAGS $PROD_FLAGS -o bin/VectorFields src/main.c $DEPS
+if [[ $1 == "d" ]] || [[ $1 == "-d" ]]; then
+	CFLAGS="$CFLAGS $DEV_FLAGS"
+else
+	CFLAGS="$CFLAGS $PROD_FLAGS"
+fi
+gcc $CFLAGS -o bin/VectorFields src/main.c $DEPS
